f7a44b3400f00b93450a5bee19e9ae35037f4211
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-02 18:18:55 -0700

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -36,9 +36,9 @@ if not os.path.isdir("./downloads"):
 
 Bot.start()
 User.start()
-print("\nVideo Player Bot Started, Join @AsmSafone!")
+print("\nVideo Player Bot Started, Join @sl_bot_zone !")
 
 idle()
 Bot.stop()
 User.stop()
-print("\nVideo Player Bot Stopped, Join @AsmSafone!")
+print("\nVideo Player Bot Stopped, Join @sl_bot_zone !")
