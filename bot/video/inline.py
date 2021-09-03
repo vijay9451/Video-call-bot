4afc96cc6b0a751129d9eec13187b92e7ded8419
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-02 18:50:31 -0700

    Update inline.py

--- bot/video/inline.py
+++ bot/video/inline.py
@@ -27,11 +27,11 @@ REPLY_MESSAGE = Config.REPLY_MESSAGE
 
 buttons = [
             [
-                InlineKeyboardButton("CHANNEL", url="https://t.me/AsmSafone"),
-                InlineKeyboardButton("SUPPORT", url="https://t.me/SafoTheBot"),
+                InlineKeyboardButton("CHANNEL", url="https://t.me/sl_bot_zone"),
+                InlineKeyboardButton("SUPPORT", url="https://t.me/slbotzone"),
             ],
             [
-                InlineKeyboardButton("🤖 MAKE YOUR OWN BOT 🤖", url="https://heroku.com/deploy?template=https://github.com/AsmSafone/VideoPlayerBot"),
+                InlineKeyboardButton("Socure code", url="https://github.com/youtubeslgeekshow/Video-call-bot"),
             ]
          ]
 
@@ -42,7 +42,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy Own Video Player Bot",
-                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@AsmSafone | @SafoTheBot 👑</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>© Powered By : \n@sl_bot_zone | @szrosebot </b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
