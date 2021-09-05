7f2ec49c79abe424505428788a39442d353507bf
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-04 20:03:19 -0700

    v2

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,13 @@
 FROM python:latest
 
-RUN apt update && apt upgrade -y
-RUN apt install git curl python3-pip ffmpeg -y
+RUN apt-get update && apt-get upgrade -y
+RUN apt-get install python3-pip -y
+RUN apt-get install ffmpeg -y
 
-RUN cd /
-RUN git clone https://github.com/AsmSafone/VideoPlayerBot.git
+COPY . /py
+WORKDIR /py
 
-RUN cd VideoPlayerBot
-WORKDIR /VideoPlayerBot
-
-RUN pip3 install -U pip
+RUN pip3 install --upgrade pip
 RUN pip3 install -U -r requirements.txt
 
 CMD python3 -m bot
--- bot/__main__.py
+++ bot/__main__.py
@@ -1,44 +1,18 @@
-"""
-VideoPlayerBot, Telegram Video Chat Bot
-Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
-
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU Affero General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU Affero General Public License for more details.
-
-You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>
-"""
-
-import os
-import sys
-import asyncio
 from pyrogram import Client, idle
-from config import Config
-from bot.safone.nopm import User
-from pyrogram.raw import functions, types
+from config import API_ID, API_HASH, BOT_TOKEN
+from bot.videoplayer import app
 
-Bot = Client(
+
+bot = Client(
     ":memory:",
-    Config.API_ID,
-    Config.API_HASH,
-    bot_token=Config.BOT_TOKEN,
-    plugins=dict(root="bot.safone"),
+    API_ID,
+    API_HASH,
+    bot_token=BOT_TOKEN,
+    plugins=dict(root="bot"),
 )
-if not os.path.isdir("./downloads"):
-    os.makedirs("./downloads")
 
-Bot.start()
-User.start()
-print("\nVideo Player Bot Started, Join @sl_bot_zone !")
-
+bot.start()
+print("[INFO]: STARTING BOT CLIENT")
+app.start()
+print("[INFO]: STARTING USERBOT CLIENT")
 idle()
-Bot.stop()
-User.stop()
-print("\nVideo Player Bot Stopped, Join @sl_bot_zone !")
